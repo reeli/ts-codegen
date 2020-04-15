756a46f0e769f4e190712516361ed111cab7f2c0
reeli <reeli.null@gmail.com>
2020-04-15 18:02:41 +0800

    create ReusableTypes to generate types from definitions or component schemas

---
+++ src/core/ReusableTypes.ts
@@ -0,0 +1,62 @@
+import { IOpenAPI } from "src/v3/OpenAPI";
+import { addPrefixForInterface, generateEnums, SchemaResolver, toCapitalCase, toTypes } from "src";
+import { compact, Dictionary, forEach, includes, isEmpty } from "lodash";
+import { ENUM_SUFFIX } from "src/core/constants";
+import { Spec } from "swagger-schema-official";
+
+export class ReusableTypes {
+  public resolvedSchemas: Dictionary<any> = {};
+
+  static of(spec: Spec | IOpenAPI) {
+    return new ReusableTypes(spec);
+  }
+
+  constructor(private spec: Spec | IOpenAPI) {}
+
+  gen = (): string[] => {
+    const r = SchemaResolver.of((k, v) => {
+      this.resolvedSchemas[k] = v;
+    });
+
+    const schemas = this.spec.definitions || (this.spec as IOpenAPI).components?.schemas;
+
+    forEach(schemas, (v, k) => {
+      r.resolve({
+        ...v,
+        _name: k,
+        _propKey: k,
+      });
+    });
+
+    const resolvedSchemas = this.resolvedSchemas;
+
+    const arr = Object.keys(resolvedSchemas)
+      .sort()
+      .map((key) => {
+        if (includes(key, ENUM_SUFFIX)) {
+          return generateEnums(resolvedSchemas, key);
+        }
+
+        if (resolvedSchemas[key] === "object") {
+          return `export interface ${addPrefixForInterface(toCapitalCase(key))} {[key:string]:any}`;
+        }
+
+        if (typeof resolvedSchemas[key] === "string") {
+          return `export type ${toCapitalCase(key)} = ${resolvedSchemas[key]}`;
+        }
+
+        if (!isEmpty(resolvedSchemas[key]?._extends)) {
+          return `export interface ${addPrefixForInterface(toCapitalCase(key))} extends ${resolvedSchemas[
+            key
+          ]?._extends.join(",")} ${toTypes(resolvedSchemas[key]?._others)} `;
+        }
+
+        const val = toTypes(resolvedSchemas[key]);
+        if (val) {
+          return `export interface ${addPrefixForInterface(toCapitalCase(key))} ${val}`;
+        }
+      });
+
+    return compact(arr)
+  };
+}
---
+++ src/core/__tests__/ReusableTypes.test.ts
@@ -0,0 +1,15 @@
+import swaggerV2 from "src/__tests__/mock-data/swagger.json";
+import swaggerV3 from "examples/swagger.v3.petstore.expanded.json";
+import { ReusableTypes } from "src/core/ReusableTypes";
+
+describe("ReusableTypes", () => {
+  it("should scan swagger v2 definitions and transform it to correct types", () => {
+    const results = ReusableTypes.of(swaggerV2 as any).gen();
+    expect(results).toMatchSnapshot();
+  });
+
+  it("should handle swagger v3 component schemas", () => {
+    const results = ReusableTypes.of(swaggerV3 as any).gen();
+    expect(results).toMatchSnapshot();
+  });
+});
