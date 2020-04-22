c5b57f81172ac0523cef016bcf5c7c489c1f84e0
reeli <reeli.null@gmail.com>
2020-04-22 12:05:16 +0800

    make prefix toggleable in ReusableTypes

--- src/core/ReusableTypes.ts
+++ src/core/ReusableTypes.ts
@@ -1,8 +1,52 @@
 import { IOpenAPI } from "src/v3/OpenAPI";
-import { generateEnums, SchemaHandler, toCapitalCase, toTypes } from "src";
-import { compact, Dictionary, forEach, includes, isEmpty } from "lodash";
+import { addPrefixForInterface, addPrefixForType, generateEnums, isArray, isObject, SchemaHandler, toTypes } from "src";
+import { compact, Dictionary, forEach, includes, isEmpty, map, mapValues } from "lodash";
 import { ENUM_SUFFIX } from "src/core/constants";
 import { Spec } from "swagger-schema-official";
+import qs from "querystring";
+
+const getQueryObjFromStr = (str: string) => {
+  const item = str.replace(/\[\]/gi, "");
+  const input = item.split("?")[1];
+  if (input) {
+    return {
+      ...qs.parse(input),
+      isArray: str.includes("[]"),
+    };
+  }
+  return {} as any;
+};
+
+interface IData {
+  _kind: string;
+  _name: string;
+  _value: string | Dictionary<any>;
+}
+
+interface IAllData {
+  [key: string]: IData;
+}
+
+const handleStr = (str: string, allData: IAllData) => {
+  const { type, name, isArray } = getQueryObjFromStr(str);
+  if (type === "ref") {
+    return isArray ? `${allData[name]._name}[]` : allData[name]._name;
+  }
+  return str;
+};
+
+const resolve = (input: string | Dictionary<any>, allData: IAllData): any => {
+  if (typeof input == "string") {
+    return handleStr(input, allData);
+  }
+  if (isArray(input)) {
+    return map(input, (v) => resolve(v, allData));
+  }
+  if (isObject(input)) {
+    return mapValues(input, (item) => resolve(item, allData));
+  }
+  return "";
+};
 
 export class ReusableTypes {
   public resolvedSchemas: Dictionary<any> = {};
@@ -13,22 +57,40 @@ export class ReusableTypes {
 
   constructor(private spec: Spec | IOpenAPI) {}
 
-  gen = (): string[] => {
+  gen = (withPrefix: boolean = true): string[] => {
     const schemaHandler = SchemaHandler.of((k, v) => {
-      this.resolvedSchemas[k] = v;
+      if (typeof v === "string") {
+        this.resolvedSchemas[k] = {
+          _kind: "type",
+          _name: withPrefix ? addPrefixForType(k) : k,
+          _value: v,
+        };
+      } else {
+        this.resolvedSchemas[k] = {
+          _kind: "interface",
+          _name: withPrefix ? addPrefixForInterface(k) : k,
+          _value: v,
+        };
+      }
     });
 
     const schemas = this.spec.definitions || (this.spec as IOpenAPI).components?.schemas;
 
     forEach(schemas, (v, k) => {
-        schemaHandler.resolve({
+      schemaHandler.resolve({
         ...v,
         _name: k,
         _propKey: k,
       });
     });
 
-    const resolvedSchemas = this.resolvedSchemas;
+    const resolvedSchemas = mapValues(this.resolvedSchemas, (item) => {
+      return {
+        _kind: item._kind,
+        _name: item._name,
+        _types: resolve(item._value, this.resolvedSchemas),
+      };
+    });
 
     const arr = Object.keys(resolvedSchemas)
       .sort()
@@ -37,23 +99,19 @@ export class ReusableTypes {
           return generateEnums(resolvedSchemas, key);
         }
 
-        if (resolvedSchemas[key] === "object") {
-          return `export interface ${toCapitalCase(key)} {[key:string]:any}`;
+        const { _name, _kind, _types } = resolvedSchemas[key];
+
+        if (!isEmpty(_types?._extends)) {
+          return `export interface ${_name} extends ${_types?._extends.join(",")} ${toTypes(_types?._others)} `;
         }
 
-        if (typeof resolvedSchemas[key] === "string") {
-          return `export type ${toCapitalCase(key)} = ${resolvedSchemas[key]}`;
+        if (_kind === "type") {
+          return `export type ${_name} = ${_types}`;
         }
 
-        if (!isEmpty(resolvedSchemas[key]?._extends)) {
-          return `export interface ${toCapitalCase(key)} extends ${resolvedSchemas[key]?._extends.join(",")} ${toTypes(
-            resolvedSchemas[key]?._others,
-          )} `;
-        }
-
-        const val = toTypes(resolvedSchemas[key]);
-        if (val) {
-          return `export interface ${toCapitalCase(key)} ${val}`;
+        if (_kind === "interface") {
+          const types = toTypes(resolvedSchemas[key]._types);
+          return `export interface ${_name} ${types}`;
         }
       });
 
--- src/core/__tests__/ReusableTypes.test.ts
+++ src/core/__tests__/ReusableTypes.test.ts
@@ -1,7 +1,7 @@
 import swaggerV2 from "examples/swagger.v2.json";
 import swaggerV3 from "examples/swagger.v3.petstore.expanded.json";
 import { ReusableTypes } from "src/core/ReusableTypes";
-import {prettifyCode} from "src/core/utils";
+import { prettifyCode } from "src/core/utils";
 
 describe("ReusableTypes", () => {
   it("should scan swagger v2 definitions and transform it to correct types", () => {
@@ -9,8 +9,13 @@ describe("ReusableTypes", () => {
     expect(prettifyCode(`${results.join("\n\n")}`)).toMatchSnapshot();
   });
 
-  it("should handle swagger v3 component schemas", () => {
+  it("should handle swagger v3 component schemas with prefix on interface or type", () => {
     const results = ReusableTypes.of(swaggerV3 as any).gen();
+    expect(prettifyCode(`${results.join("\n\n")}`)).toMatchSnapshot();
+  });
+
+  it("should handle swagger v3 component schemas without prefix on interface or type", () => {
+    const results = ReusableTypes.of(swaggerV3 as any).gen(false);
     expect(prettifyCode(`${results.join("\n\n")}`)).toMatchSnapshot();
   });
 });
