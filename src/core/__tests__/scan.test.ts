2267f3c2b9d9f0fe66b6a4aa4192165c12a45852
reeli <reeli.null@gmail.com>
2020-05-05 17:11:45 +0800

    update test

--- src/core/__tests__/scan.test.ts
+++ src/core/__tests__/scan.test.ts
@@ -1,18 +1,18 @@
-import { scan } from "src/core/scan";
-import { CustomSchema } from "src/core/Type";
 import swaggerV2 from "examples/swagger.v2.json";
 import swaggerV3 from "examples/swagger.v3.petstore.expanded.json";
+import { Scanner } from "src/core/Scanner";
+import { Spec } from "swagger-schema-official";
+import { IOpenAPI } from "src/v3/OpenAPI";
 
 describe("swagger v2", () => {
   it("should handle definitions correctly", () => {
-    const v = scan(swaggerV2.definitions as { [k: string]: CustomSchema });
-    expect(v).toMatchSnapshot();
+    expect(new Scanner(swaggerV2 as Spec).scan()).toMatchSnapshot();
   });
 });
 
 describe("swagger v3", () => {
   it("should handle schemas correctly", () => {
-    const v = scan(swaggerV3?.components.schemas as { [k: string]: CustomSchema });
+    const v = new Scanner(swaggerV3 as IOpenAPI);
     expect(v).toMatchSnapshot();
   });
 });
