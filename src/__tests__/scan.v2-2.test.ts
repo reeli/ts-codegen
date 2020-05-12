cc3c23630316f57ecd806b8ef080f02e6a4bb076
reeli <reeli.null@gmail.com>
2020-05-12 16:59:56 +0800

    update test cases

--- src/__tests__/scan.v2-2.test.ts
+++
@@ -1,8 +0,0 @@
-import swaggerV2PetStore from "examples/petstore.json";
-import { scan } from "src/scan";
-
-describe("swagger v2", () => {
-  it("should handle special spec correctly", () => {
-    expect(scan(swaggerV2PetStore as any)).toMatchSnapshot();
-  });
-});
--- src/__tests__/scan.v3-2.test.ts
+++
@@ -1,9 +0,0 @@
-import swaggerV3PetStore from "examples/petstore.v3.json";
-import { scan } from "src/scan";
-import { IOpenAPI } from "src/__types__/OpenAPI";
-
-describe("swagger v3", () => {
-  it("should handle special schemas correctly", () => {
-    expect(scan(swaggerV3PetStore as IOpenAPI)).toMatchSnapshot();
-  });
-});
