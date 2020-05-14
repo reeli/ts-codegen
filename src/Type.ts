9b0e33b4903820d3943fca0c8dc3520d2030b4b4
reeli <reeli.null@gmail.com>
2020-05-14 21:48:57 +0800

    refactor code

--- src/Type.ts
+++ src/Type.ts
@@ -4,66 +4,46 @@ import { createRegister, DeclKinds } from "src/createRegister";
 
 export type CustomType = Ref | Obj | Arr | Enum | OneOf | BasicType;
 
-abstract class TypeFactory {
-  abstract toType(): string;
+interface TypeFactory {
+  toType: () => string;
 }
 
-class BasicType extends TypeFactory {
+class BasicType implements TypeFactory {
   static type(name: string) {
     return new BasicType(name);
   }
 
-  constructor(private name: string) {
-    super();
-  }
+  constructor(private name: string) {}
 
   toType() {
     return this.name;
   }
 }
 
-export class Enum extends TypeFactory {
-  alias: string | undefined;
-  constructor(private id: string, private value?: any[], private kind?: DeclKinds) {
-    super();
-  }
-
-  rename(alias: string) {
-    this.alias = alias;
-  }
+export class Enum implements TypeFactory {
+  constructor(private name: string, private value?: any[], private kind?: DeclKinds) {}
 
   toType(): string {
     if (this.value) {
       if (this.kind === DeclKinds.type) {
         return `${this.value.map((v) => `"${v}"`).join("|")}`;
       }
-      return `{
-      ${this.value
-        .map((v) => {
-          return `'${v}' = '${v}',`;
-        })
-        .join("\n")}
-      }`;
+      return `{${this.value.map((v) => `'${v}' = '${v}',`).join("\n")}}`;
     }
-    return this.kind === DeclKinds.type ? this.alias || this.id : `keyof typeof ${this.id}`;
+    return `keyof typeof ${this.name}`;
   }
 }
 
-class OneOf extends TypeFactory {
-  constructor(private types: CustomType[]) {
-    super();
-  }
+class OneOf implements TypeFactory {
+  constructor(private types: CustomType[]) {}
 
   toType(): string {
     return `${map(this.types, (type) => type.toType()).join("|")}`;
   }
 }
 
-export class Arr extends TypeFactory {
-  // TODO: remove any later
-  constructor(private data: CustomType[] | CustomType) {
-    super();
-  }
+export class Arr implements TypeFactory {
+  constructor(private data: CustomType[] | CustomType) {}
 
   toType(): string {
     if (isArray(this.data)) {
@@ -73,12 +53,10 @@ export class Arr extends TypeFactory {
   }
 }
 
-export class Ref extends TypeFactory {
+export class Ref implements TypeFactory {
   alias: string | undefined;
 
-  constructor(private name: string) {
-    super();
-  }
+  constructor(private name: string) {}
 
   rename(alias: string) {
     this.alias = alias;
@@ -89,14 +67,12 @@ export class Ref extends TypeFactory {
   }
 }
 
-export class Obj extends TypeFactory {
+export class Obj implements TypeFactory {
   constructor(
     private props: { [key: string]: CustomType } | string,
     private refs?: Ref[],
     private useExtends?: boolean,
-  ) {
-    super();
-  }
+  ) {}
 
   toType(useExtends = this.useExtends): string {
     if (this.props === "object") {
@@ -104,21 +80,19 @@ export class Obj extends TypeFactory {
     }
 
     const handler = (props: { [key: string]: CustomType } | CustomType): string => {
-      // //TODO: refactor next line later
       if (props?.toType) {
         return (props as CustomType).toType();
       }
+
       const data = keys(props)
         .sort()
-        .map((k) => {
-          // TODO: remove any later
-          return `${quoteKey(k)}: ${(props as any)[k].toType()};`;
-        });
+        .map((k) => `${quoteKey(k)}: ${(props as { [key: string]: CustomType })[k].toType()};`);
+
       return `{${data.join("")}}`;
     };
+
     if (!isEmpty(this.refs)) {
       if (isEmpty(this.props)) {
-        // TODO: handle this case and add test for it
         if (!useExtends) {
           return map(this.refs, (v) => v.toType()).join("&");
         }
@@ -130,21 +104,24 @@ export class Obj extends TypeFactory {
           )}`
         : `${map(this.refs, (v) => v.toType()).join("&")}&${handler(this.props as { [key: string]: CustomType })}`;
     }
+
     return handler(this.props as { [key: string]: CustomType });
   }
 }
 
+const hasNumber = (list: any[]) => some(list, (v) => isNumberLike(v));
+
 export class Type {
-  //TODO: 解决 id 重名的问题
   constructor(private register: ReturnType<typeof createRegister>) {}
 
   enum(value: any[], id: string = uniqueId("Enum")) {
-    const hasNumber = (list: any[]) => some(list, (v) => isNumberLike(v));
     const kind = hasNumber(value) ? DeclKinds.type : DeclKinds.enum;
     this.register.setDecl(id, new Enum(id, value, kind), kind);
+
     if (kind === DeclKinds.type) {
       return this.register.setRef(id);
     }
+
     return new Enum(id, undefined, kind);
   }
 
